e065c95b935ac7d9f2d2217a42e348df777a875b
unknown <rbilderbeek@Luchtlager.schut.com>
2013-06-03 08:55:13 +0200

    Added int64_t support

--- Tools/ToolStateObserver/lsqfilter.cpp
+++ Tools/ToolStateObserver/lsqfilter.cpp
@@ -12,17 +12,17 @@
 
 LsqFilter::LsqFilter(
   const int filter_shift,
-  const int value_active,
-  const int value_hidden)
+  const int64_t value_active,
+  const int64_t value_hidden)
   : m_val1(value_hidden),
     m_val2(value_active),
     m_filter_shift(filter_shift)
 {
   assert(m_filter_shift >=  0 && "A bitshift should not be done with negative values");
-  assert(m_filter_shift <= 31 && "An int can maximally be shifted 31 bits to the right");
+  assert(m_filter_shift <= 63 && "An int can maximally be shifted 63 bits to the right");
 }
 
-int LsqFilter::Estimate(const int measurement)
+int64_t LsqFilter::Estimate(const int64_t measurement)
 {
   m_val1 += ((measurement  - m_val1) >> m_filter_shift);
   m_val2 += ((m_val1 - m_val2) >> m_filter_shift);
--- Tools/ToolStateObserver/lsqfilter.h
+++ Tools/ToolStateObserver/lsqfilter.h
@@ -1,19 +1,21 @@
 #ifndef LSQFILTER_H
 #define LSQFILTER_H
 
+#include <cinttypes>
+
 struct LsqFilter
 {
   LsqFilter(
     const int filter_shift,
-    const int value_active = 0,
-    const int value_hidden = 0);
+    const int64_t value_active = 0,
+    const int64_t value_hidden = 0);
 
-  int Estimate(const int measurement);
+  int64_t Estimate(const int64_t measurement);
 
   private:
-  int m_val1;
-  int m_val2;
-  int m_filter_shift;
+  int64_t m_val1;
+  int64_t m_val2;
+  int64_t m_filter_shift;
 };
 
 #endif // LSQFILTER_H
