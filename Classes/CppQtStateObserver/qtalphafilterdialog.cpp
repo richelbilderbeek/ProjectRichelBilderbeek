a528ddea019516697e12045149dd2be6604e84ee
Richel Bilderbeek <richelbilderbeek@gmail.com>
2013-06-18 15:48:47 +0200

    StateObserver: second step in making state observer dialogs

--- Classes/CppQtStateObserver/qtalphafilterdialog.cpp
+++ Classes/CppQtStateObserver/qtalphafilterdialog.cpp
@@ -7,6 +7,8 @@
 // * John Lakos. Large-Scale C++ Software Design. 1996. ISBN: 0-201-63362-0. Section 3.2, page 110
 #include "qtalphafilterdialog.h"
 
+#include <cassert>
+#include "alphafilter.h"
 #include "ui_qtalphafilterdialog.h"
 
 QtAlphaFilterDialog::QtAlphaFilterDialog(QWidget *parent) :
@@ -19,4 +21,13 @@ QtAlphaFilterDialog::QtAlphaFilterDialog(QWidget *parent) :
 QtAlphaFilterDialog::~QtAlphaFilterDialog()
 {
   delete ui;
+}
+
+boost::shared_ptr<AlphaFilter> QtAlphaFilterDialog::Create() const
+{
+  const double alpha  = ui->box_alpha->value();
+  const double dt =  ui->box_dt->value();
+  const boost::shared_ptr<AlphaFilter> filter(new AlphaFilter(alpha,dt));
+  assert(filter);
+  return filter;
 }
