635a8af2546393e24e4231cfa83b3a5c2ab3b183
Richel Bilderbeek <richelbilderbeek@gmail.com>
2013-07-10 22:16:44 +0200

    Added libcvautomation example 6

---
+++ Test/CppLibcvautomationExample6/CppLibcvautomationExample6.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-07-10T21:02:16
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = CppLibcvautomationExample6
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        dialog.cpp
+
+HEADERS  += dialog.h
+
+FORMS    += dialog.ui
---
+++ Test/CppLibcvautomationExample6/dialog.h
@@ -0,0 +1,28 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+  class Dialog;
+}
+
+class Dialog : public QDialog
+{
+  Q_OBJECT
+  
+public:
+  explicit Dialog(QWidget *parent = 0);
+  ~Dialog();
+  
+private slots:
+
+  void on_pushButton_1_clicked();
+  void on_pushButton_2_clicked();
+  void on_pushButton_3_clicked();
+
+private:
+  Ui::Dialog *ui;
+};
+
+#endif // DIALOG_H
