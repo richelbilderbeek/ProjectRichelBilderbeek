be0e1c08f7cc2e1e7765647ed53b3124daa774c2
Richel Bilderbeek <richelbilderbeek@gmail.com>
2013-07-24 12:03:48 +0200

    Improved libcvautomation install script

--- Libraries/libcvautomation.sh
+++ Libraries/libcvautomation.sh
@@ -12,3 +12,10 @@ cd libcvautomation
 ./autogen.sh
 ./configure
 make
+
+if [ -e ./examples/cva-input ]
+then
+  echo "libcvautomation installation: SUCCESS"
+else
+  echo "libcvautomation installation: FAIL (./examples/cva-input not found"
+fi 
