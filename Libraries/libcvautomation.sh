66864f133ea858cf31d1a8e0a014931cbcf2bbba
Richel Bilderbeek <richelbilderbeek@gmail.com>
2013-07-24 12:00:23 +0200

    libcvautomation: added install to script

--- Libraries/libcvautomation.sh
+++ Libraries/libcvautomation.sh
@@ -1,2 +1,14 @@
-#!/bin/sh
-git clone https://github.com/richelbilderbeek/libcvautomation.git
+#!/bin/bash
+if [ ! -d libcvautomation ]
+then
+  echo "Cloning libcvautomation"
+  git clone https://github.com/richelbilderbeek/libcvautomation.git
+else
+  echo "libcvautomation already present"
+fi
+
+cd libcvautomation
+
+./autogen.sh
+./configure
+make
