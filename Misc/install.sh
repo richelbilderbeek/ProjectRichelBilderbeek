09adead72bcb02e149b027333128da00a8a763d2
Richel Bilderbeek <richelbilderbeek@gmail.com>
2013-07-28 06:21:53 +0200

    Added MXE prerequisites by copy-paste

--- Misc/install.sh
+++ Misc/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 apt-get install git vim qtcreator tmux make g++ libboost-all-dev witty-dev libqwt-dev autoconf bison cmake flex intltool libtool pkg-config scons tidy libopencv-dev lynx lyx libxtst-dev
 
+#From MXE
+apt-get install autoconf automake bash bison bzip2 \
+                    cmake flex gettext git g++ intltool \
+                    libffi-dev libtool libltdl-dev libssl-dev \
+                    libxml-parser-perl make openssl patch perl \
+                    pkg-config scons sed unzip wget xz-utils
 cd ~/
 if [ ! -d "ProjectRichelBilderbeek" ]
 then
