9c96646c9fc15bb2590c6e90256581aabcd5d379
Richel Bilderbeek <richelbilderbeek@gmail.com>
2013-08-25 19:52:05 +0200

    Misc

--- Misc/install.sh
+++ Misc/install.sh
@@ -15,3 +15,33 @@ else
   echo "git repo already downloaded"
 fi
 
+#Check libraries
+cd ProjectRichelBilderbeek/Libraries
+
+if [ ! -d "bigint-2010.04.30" ]
+then
+  ./bigint.sh
+fi
+
+if [ ! -d "libcvautomation" ]
+then
+  ./libcvautomation.sh
+fi
+
+
+if [ ! -d "fparser4.4.3" ]
+then
+  ./fparser.sh
+fi
+
+if [ ! -d "fparser4.5.1" ]
+then
+  ./fparser.sh
+fi
+
+if [ ! -d "mxe" ]
+then
+  ./mxe.sh
+fi 
+
+echo "Installation done"
