b2091cb284628621c84c0ecbdc7ea338a56e201a
richelbilderbeek <richelbilderbeek@gmail.com>
2013-11-02 17:22:47 +0100

    Improved acv-libc script

--- Libraries/avr-libc.sh
+++ Libraries/avr-libc.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-bzip2 -d avr-libc-1.8.0.tar.bz2
+if [ ! -d avr-libc-1.8.0 ]
+then
+  echo "Extracting avr-libc-1.8.0"
+  tar xjvf avr-libc-1.8.0.tar.bz2
+else
+  echo "avr-libc-1.8.0 already extracted"
+fi
+
+cd avr-libc-1.8.0
+
+if [ ! -f "Makefile" ]
+then
+  echo "Makefile not present, call configure to create it"
+  ./configure --build=`./config.guess` --host=avr
+else
+  echo "Makefile present"
+fi
+
+make
+make install
