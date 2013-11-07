704d64242328aac79958c8417f479a755e1f08ac
richelbilderbeek <richelbilderbeek@gmail.com>
2013-11-07 10:09:32 +0100

    Added pri files

---
+++ Games/GamePaperRockScissors/GamePaperRockScissorsConsole.pri
@@ -0,0 +1,14 @@
+INCLUDEPATH += \
+    ../../Games/GameBeerWanter
+
+SOURCES += \
+    ../../Games/GameBeerWanter/beerwantermaindialog.cpp \
+    ../../Games/GameBeerWanter/beerwantermenudialog.cpp
+
+HEADERS  += \
+    ../../Games/GameBeerWanter/beerwantermaindialog.h \
+    ../../Games/GameBeerWanter/beerwantermenudialog.h
+
+OTHER_FILES += \
+    ../../Games/GameBeerWanter/Licence.txt
+
---
+++ Games/GameTankBattalion/tankbattalionmenudialog.h
@@ -0,0 +1,43 @@
+//---------------------------------------------------------------------------
+/*
+BeerWanter. A simple game.
+Copyright (C) 2005-2013 Richel Bilderbeek
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see <http://www.gnu.org/licenses/>.
+*/
+//---------------------------------------------------------------------------
+//From http://www.richelbilderbeek.nl/GameBeerWanter.htm
+//---------------------------------------------------------------------------
+#ifndef BEERWANTERMENUDIALOG_H
+#define BEERWANTERMENUDIALOG_H
+
+#include "menudialog.h"
+
+namespace ribi {
+
+struct BeerWanterMenuDialog : public MenuDialog
+{
+  const About GetAbout() const noexcept;
+  const Help GetHelp() const noexcept;
+  const boost::shared_ptr<const Program> GetProgram() const noexcept;
+  const std::string GetVersion() const noexcept;
+  const std::vector<std::string> GetVersionHistory() const noexcept;
+
+  private:
+  int ExecuteSpecific(const std::vector<std::string>& argv) noexcept;
+};
+
+} //~namespace ribi
+
+#endif // BEERWANTERMENUDIALOG_H
