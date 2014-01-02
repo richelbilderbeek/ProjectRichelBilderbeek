65d290245815128898c0df8d7679dabe20078cc7
richelbilderbeek <richelbilderbeek@gmail.com>
2014-01-02 11:04:23 +0100

    OpenFOAM example 19

--- Test/ToolOpenFoamExample19/Clean.bat
+++ Test/ToolOpenFoamExample19/Clean.bat
@@ -1,9 +1,9 @@
-del constant\polyMesh\boundary
-del constant\polyMesh\cellZones
-del constant\polyMesh\faces
-del constant\polyMesh\neighbour
-del constant\polyMesh\owner
-del constant\polyMesh\points
-del constant\polyMesh\sets\inlet
-del constant\polyMesh\sets\outlet
-rmdir constant\polyMesh\sets
+del /q /s 1
+del /q /s 2
+del /q /s constant\polyMesh
+
+rmdir 1\polyMesh
+rmdir 2\polyMesh
+rmdir 1
+rmdir 2
+rmdir constant\polyMesh
--- Test/ToolOpenFoamExample19/ToolOpenFoamExample19.bat
+++ Test/ToolOpenFoamExample19/ToolOpenFoamExample19.bat
@@ -1,1 +1,7 @@
-hexSnappyMesh
+xcopy /y constant\polyMeshCube\*.* constant\polyMesh\*.* 
+blockMesh
+surfaceFeatureExtract -includedAngle 150 constant/triSurface/mymesh.stl mymeshfeatures
+snappyHexMesh
+xcopy /y 2\polyMesh\*.* constant\polyMesh\*.*
+
+
