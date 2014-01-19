87ad24ba411c40763a4b3f5ca0aaad3c3e163d2e
Richel Bilderbeek <richelbilderbeek@gmail.com>
2014-01-19 18:09:59 +0100

    Trivial merge

--- Test/ArduinoHbridge4pinWithStatusExperimenting/ArduinoHbridge4pinWithStatusExperimenting.ino
+++ Test/ArduinoHbridge4pinWithStatusExperimenting/ArduinoHbridge4pinWithStatusExperimenting.ino
@@ -1,38 +1,3 @@
-//  +-V_high -+
-//  |         |
-//  3         4
-//  |         |
-//  +---M-----+
-//  |         |
-//  1         2
-//  |         |
-//  +-V_low---+
-//
-// Route A
-//
-//  +-V_high -+
-//            |
-//            4
-//            |
-//  +---M-----+
-//  |          
-//  1          
-//  |          
-//  +-V_low---+
-//
-// Route A
-//
-//  +-V_high -+
-//  |          
-//  3          
-//  |          
-//  +---M-----+
-//            |
-//            2
-//            |
-//  +-V_low---+
-
-
 const int pin_1 = 2;
 const int pin_2 = 3;
 const int pin_3 = 4;
@@ -45,43 +10,15 @@ void setup()
   pinMode(pin_2,OUTPUT);
   pinMode(pin_3,OUTPUT);
   pinMode(pin_4,OUTPUT);
-  
-  pinMode(pin_status,INPUT);
-
-  Serial.begin(9600);
-
 }
 
 void loop()
 {
-  
-  //The time between a change in current direction
-  const int my_toggle_delay = 10000; //msec
-  //Route A
-  
-  digitalWrite(pin_1,LOW);
-  digitalWrite(pin_2,LOW);
-  digitalWrite(pin_3,LOW);
-  digitalWrite(pin_4,LOW);
+  digitalWrite(pin_1,(rand() >> 4) % 2 ? LOW : HIGH);
+  digitalWrite(pin_2,(rand() >> 4) % 2 ? LOW : HIGH);
+  digitalWrite(pin_3,(rand() >> 4) % 2 ? LOW : HIGH);
+  digitalWrite(pin_4,(rand() >> 4) % 2 ? LOW : HIGH);
 
-  {
-    const bool ok = digitalRead(pin_status) == HIGH;
-    Serial.print(ok ? "." : "ERROR\n");
-  }
-  delay(my_toggle_delay);
-  
-  //Route B
-  /*
-  digitalWrite(pin_1,HIGH);
-  digitalWrite(pin_2,HIGH);
-  digitalWrite(pin_3,HIGH);
-  digitalWrite(pin_4,HIGH);
+  delay(100);
 
-  {
-    const bool ok = digitalRead(pin_status) == HIGH;
-    Serial.print(ok ? "." : "ERROR\n");
-  }
-
-  delay(my_toggle_delay);
-  */
 }
