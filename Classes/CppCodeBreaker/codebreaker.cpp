4e691f6157d95fec18dfb7a6060a120015dbab02
richelbilderbeek <richelbilderbeek@gmail.com>
2014-06-06 07:42:54 +0200

    Fixed #202

--- Classes/CppCodeBreaker/codebreaker.cpp
+++ Classes/CppCodeBreaker/codebreaker.cpp
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------------
 /*
 CodeBreaker, code breaking class
-Copyright (C) 2010-2014 Richel Bilderbeek
+Copyright (C) 2014-2014 Richel Bilderbeek
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
