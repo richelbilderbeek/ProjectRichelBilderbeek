bc62604d4053f7d5df20f7d03a9a79bb6bb5dbf7
Richel Bilderbeek <richelbilderbeek@users.noreply.github.com>
2014-09-02 14:36:52 +0000

    Update install.sh

--- Misc/install.sh
+++ Misc/install.sh
@@ -3,7 +3,7 @@ apt-get install git vim qtcreator tmux make g++ libboost-all-dev witty-dev libqw
 
 #From MXE
 apt-get install autoconf automake autopoint bash bison bzip2 \
-                    cmake flex gettext git g++ intltool \
+                    cmake flex gettext git g++ gperf intltool \
                     libffi-dev libtool libltdl-dev libssl-dev \
                     libxml-parser-perl make openssl patch perl \
                     pkg-config scons sed unzip wget xz-utils
