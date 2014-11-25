fb555d496ac5866af4b26f210e71c312c680bbdd
richelbilderbeek <richel@richelbilderbeek.nl>
2014-11-25 21:58:59 +0100

    Removed use of personal libcvautomation

--- Libraries/libcvautomation.sh
+++ Libraries/libcvautomation.sh
@@ -1,21 +1,2 @@
-#!/bin/bash
-if [ ! -d libcvautomation ]
-then
-  echo "Cloning libcvautomation"
-  git clone https://github.com/richelbilderbeek/libcvautomation.git
-else
-  echo "libcvautomation already present"
-fi
-
-cd libcvautomation
-
-./autogen.sh
-./configure
-make
-
-if [ -e ./examples/cva-input ]
-then
-  echo "libcvautomation installation: SUCCESS"
-else
-  echo "libcvautomation installation: FAIL (./examples/cva-input not found"
-fi 
+#!/bin/sh
+git clone https://github.com/DjBushido/libcvautomation.git
