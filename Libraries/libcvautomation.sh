3cd6be5d6fc3058d867441f1e4cc325a894a3c8d
richelbilderbeek <richel@richelbilderbeek.nl>
2014-12-08 20:03:09 +0100

    Compile more programs

--- Libraries/libcvautomation.sh
+++ Libraries/libcvautomation.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-git clone https://github.com/DjBushido/libcvautomation.git
+
+if [ ! -d libcvautomation ]
+then
+  echo "Cloning libcvautomation"
+  git clone https://github.com/DjBushido/libcvautomation.git
+else
+  echo "libcvautomation already cloned"
+fi
+
+cd libcvautomation
+./configure
+make
+make install
+
