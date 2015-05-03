cf73b74a3eacf13852cc136080bc9e556af25662
richelbilderbeek <richel@richelbilderbeek.nl>
2015-05-03 12:13:22 +0200

    Before changing TreeNode m_parent

--- Tools/ToolRosindellEtAl2008/treenode.h
+++ Tools/ToolRosindellEtAl2008/treenode.h
@@ -8,10 +8,13 @@ struct TreeNode
 // an array of these objects is required to store the whole tree
 // we include a few simple functions that prove useful later
 {
+  using Parent = int;
+  //using Parent = TreeNode*;
+
   TreeNode(const bool root);
-  void SetParent(const int parent) { m_parent = parent; }
+  void SetParent(const Parent parent) { m_parent = parent; }
   bool GetRoot() const noexcept { return m_root; }
-  int GetParent() const noexcept { return m_parent; }
+  Parent GetParent() const noexcept { return m_parent; }
   int GetSteps() const noexcept { return m_steps; }
   void IncSteps() noexcept { ++m_steps; }
 private:
@@ -23,7 +26,7 @@ private:
   /// this stores the parent of the individual
   /// 0 means there is no parent - we are at the end of the tree
   /// (as far as has been calculated)
-  int m_parent;
+  Parent m_parent;
 
   /// the number of generations (chances of speciation)
   /// between this individual and its parent in the tree
