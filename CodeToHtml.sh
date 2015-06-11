c445d95c965ed69ac853b8369f6bc68d97665618
richelbilderbeek <richel@richelbilderbeek.nl>
2015-06-11 06:46:04 +0200

    Status

--- CodeToHtml.sh
+++ CodeToHtml.sh
@@ -3,27 +3,27 @@ set -u
 
 cd Classes
 ./CodeToHtml.sh
-mv *.htm ../Projects/RichelbilderbeekNl
+mv *.htm ../../RichelbilderbeekNl/Cpp
 cd ..
 
 cd Games
 ./CodeToHtml.sh
-mv *.htm ../Projects/RichelbilderbeekNl
+mv *.htm ../../RichelbilderbeekNl/Games
 cd ..
 
 cd Projects
 ./CodeToHtml.sh
-mv *.htm ../Projects/RichelbilderbeekNl
+mv *.htm ../../RichelbilderbeekNl/Pojects
 cd ..
 
 cd Test
 ./CodeToHtml.sh
-mv *.htm ../Projects/RichelbilderbeekNl
+mv *.htm ../../RichelbilderbeekNl/Cpp
 cd ..
 
 cd Tools
 ./CodeToHtml.sh
-mv *.htm ../Projects/RichelbilderbeekNl
+mv *.htm ../../RichelbilderbeekNl/Tools
 cd ..
 
 
