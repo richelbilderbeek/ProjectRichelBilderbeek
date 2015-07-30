025de1823a0b7a3f3f49b589ef5aa63525005a7c
richelbilderbeek <richel@richelbilderbeek.nl>
2015-07-30 11:26:40 +0200

    Detect absence of DEVKITPRO

--- Libraries/libnds.sh
+++ Libraries/libnds.sh
@@ -14,3 +14,13 @@ fi
 # make
 # make install
 
+if [ ${#DEVKITPRO} -eq 0 ]
+then
+  echo "ERROR: variable DEVKITPRO not defined"
+else
+  echo "OK: variable DEVKITPRO defined"
+fi
+
+#ifeq ($(strip $(DEVKITPRO)),)
+#$(error "Please set DEVKITPRO in your environment. export DEVKITPRO=<path to>devkitPro)
+#endif
