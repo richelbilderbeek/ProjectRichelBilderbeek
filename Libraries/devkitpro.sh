d3b8483a71a475a1467dfa94ffec281dab526fa2
richelbilderbeek <richel@richelbilderbeek.nl>
2015-07-30 19:38:00 +0200

    SCripts work

--- Libraries/devkitpro.sh
+++ Libraries/devkitpro.sh
@@ -1,16 +1,21 @@
-#git clone https://github.com/devkitPro/installer.git devkitpro-installer
-#cd devkitpro-installer
-#cd perl
-#./devkitARMupdate.pl
+git clone https://github.com/devkitPro/installer.git devkitpro-installer
+cd devkitpro-installer
+cd perl
+./devkitARMupdate.pl
 
-cd ~/devkitPro
-export DEVKITPRO=$PWD
-cd devkitARM
-export DEVKITARM=$PWD
-
+# Prevent this libnds message:
 # Checking DEVKITPRO...Please set DEVKITPRO in your environment as /home/richel/devkitPro
 # Checking DEVKITARM...Please set DEVKITARM in your environment as ${DEVKITPRO}/devkitARM
+#
+# Adding those to the .bashrc file
 
-echo "DEVKITPRO: "$DEVKITPRO
-echo "DEVKITARM: "$DEVKITARM
+cd ~/devkitPro
+echo "export DEVKITPRO=$PWD" >> ~/.bashrc
+cd devkitARM
+echo "export DEVKITARM=$PWD" >> ~/.bashrc
+
+echo "Added DEVKITPRO and DEVKITARM to .bashrc"
+echo "Please close bash now to make this work,"
+echo "then start libnds.sh"
+
 
--- Libraries/libnds.sh
+++ Libraries/libnds.sh
@@ -8,45 +8,12 @@ else
   echo "libnds already cloned"
 fi
 
-# cd libcvautomation
-# ./autogen.sh
-# ./configure
-# make
-# make install
-
 if [ ${#DEVKITPRO} -eq 0 ]
 then
-  echo "Variable DEVKITPRO not defined, defining it as "$DEVKITPRO
-  export DEVKITPRO=$PWD
+  echo "Variable DEVKITPRO not defined, installing devkitpro"
+  ./devkitpro.sh
 else
   echo "OK: variable DEVKITPRO defined"
-fi
-
-if [ ${#DEVKITPRO} -eq 0 ]
-then
-  echo "ERROR: variable DEVKITPRO not defined"
-  exit
-else
-  echo "OK: variable DEVKITPRO defined as "$DEVKITPRO
-fi
-
-
-
-
-if [ ${#DEVKITARM} -eq 0 ]
-then
-  echo "Variable DEVKITARM not defined, defining it as "$DEVKITARM
-  export DEVKITARM=$PWD
-else
-  echo "OK: variable DEVKITARM defined"
-fi
-
-if [ ${#DEVKITARM} -eq 0 ]
-then
-  echo "ERROR: variable DEVKITARM not defined"
-  exit
-else
-  echo "OK: variable DEVKITARM defined as "$DEVKITARM
 fi
 
 cd libnds
