d610806b7decb5aae710db8327318ebf47538513
richelbilderbeek <richel@richelbilderbeek.nl>
2015-09-18 09:28:19 +0200

    Also install help

--- Misc/install_packages.sh
+++ Misc/install_packages.sh
@@ -4,6 +4,13 @@ apt-get install \
   git \
   vim \
   qtcreator \
+  qt4-demos \
+  qt4-doc \
+  qt4-doc-html \
+  qt5-doc \
+  qt5-doc-html \
+  qtbase5-examples \
+  qtbase5-doc-html \
   tmux \
   make \
   g++ \
@@ -30,8 +37,31 @@ apt-get install \
   libbullet-dev
 
 #From MXE
-apt-get install autoconf automake autopoint bash bison bzip2 \
-                    cmake flex gettext git g++ gperf intltool \
-                    libffi-dev libtool libltdl-dev libssl-dev \
-                    libxml-parser-perl make openssl patch perl \
-                    pkg-config scons sed unzip wget xz-utils 
+apt-get install \
+  autoconf \
+  automake \
+  autopoint \
+  bash \
+  bison \
+  bzip2 \
+  cmake \
+  flex \
+  gettext \ 
+  git \
+  gperf \
+  intltool \
+  libffi-dev \
+  libtool \
+  libltdl-dev \
+  libssl-dev \
+  libxml-parser-perl \
+  make \
+  openssl \
+  patch \
+  perl \
+  pkg-config \
+  scons \
+  sed \
+  unzip \
+  wget \
+  xz-utils 
