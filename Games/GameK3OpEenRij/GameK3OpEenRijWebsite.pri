300248e3be7f5c91eb9d44c23883ffa29eee7e8a
richelbilderbeek <richel@richelbilderbeek.nl>
2015-10-02 14:40:01 +0200

    Moved K3OpEenRij to own repo

--- Games/GameK3OpEenRij/GameK3OpEenRijWebsite.pri
+++
@@ -1,5 +0,0 @@
-include(../../Games/GameK3OpEenRij/GameK3OpEenRijConsole.pri)
-
-#Moved to GameK3OpEenRijConsole.pri
-# RESOURCES += \
-#     ../../Games/GameK3OpEenRij/GameK3OpEenRij.qrc
--- Games/GameK3OpEenRij/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include "k3opeenrijmenudialog.h"
-
-int main(int argc, char * argv[])
-{
-  const std::vector<std::string> args { ribi::MenuDialog::ConvertArguments(argc,argv) };
-  return ribi::K3OpEenRijMenuDialog().Execute(args);
-}
