ba1dc8151d38856babb18183dedb1bf873d60238
richelbilderbeek <richel@richelbilderbeek.nl>
2015-10-04 13:29:43 +0200

    Moved Hometrainer

--- Tools/ToolHometrainer/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "hometrainermenudialog.h"
-#include "hometrainermaindialog.h"
-
-int main(int argc, char * argv[])
-{
-  const std::vector<std::string> args { ribi::MenuDialog::ConvertArguments(argc,argv) };
-  return ribi::HometrainerMenuDialog().Execute(args);
-}
